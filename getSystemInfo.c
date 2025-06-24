7f825bd0a136f191e09d8b605efa3351ce93a0d8
Ridful <19477176+Ridful@users.noreply.github.com>
2025-06-24 19:17:18 +0200

    Now gets Processor information & local time & timezone information

--- getSystemInfo.c
+++ getSystemInfo.c
@@ -4,6 +4,8 @@
 #include <Windows.h>
 #include <tchar.h>
 #include <lmcons.h> //for UNLEN
+//#include <winnls.h> //for GetUserDefaultGeoName & GetSystemDefaultGeoName
+#include <locale.h>
 
 int main() {
 
@@ -23,4 +25,64 @@ int main() {
     if (GetUserName(userName, &userSize)) {
         printf("UserName: %s\n", userName);
     }
+
+    /*
+    WCHAR userGeoName[10];
+    DWORD userGeoNameSize = sizeof(userGeoName) / sizeof(WCHAR);
+    if (GetUserDefaultGeoName(userGeoName, userGeoNameSize)) {
+        printf("userGeoName: %s\n", userGeoName);
+    }*/
+
+
+    //Get information about the processor
+    SYSTEM_INFO sysInfo;
+    GetSystemInfo(&sysInfo);
+
+    printf("Number of processors: %u\n", sysInfo.dwNumberOfProcessors);
+    printf("Page Size: %u bytes\n", sysInfo.dwPageSize);
+    printf("Processor Type: %u\n", sysInfo.dwProcessorType);
+
+    printf("Processor Architecture: ");
+    switch (sysInfo.wProcessorArchitecture) {
+
+        case PROCESSOR_ARCHITECTURE_AMD64:
+            printf("x64 (AMD or Intel)\n");
+            break;
+
+        case PROCESSOR_ARCHITECTURE_ARM:
+            printf("ARM\n");
+            break;
+
+        case PROCESSOR_ARCHITECTURE_INTEL:
+            printf("x86\n");
+            break;
+
+        default:
+            printf("Unknown Architecture");
+            break;
+    }
+
+    // Get local time
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+
+    printf("Local time: %02d-%02d-%04d %02d:%02d:%02d\n",
+        st.wDay, st.wMonth, st.wYear,
+        st.wHour, st.wMinute, st.wSecond);
+
+    // Get time zone info
+    TIME_ZONE_INFORMATION tzi;
+    DWORD result = GetTimeZoneInformation(&tzi);
+
+    // Print time zone name
+    if (result == TIME_ZONE_ID_STANDARD || result == TIME_ZONE_ID_UNKNOWN) {
+        wprintf(L"Time zone (Standard): %s\n", tzi.StandardName);
+    } else if (result == TIME_ZONE_ID_DAYLIGHT) {
+        wprintf(L"Time zone (Daylight): %s\n", tzi.DaylightName);
+    } else {
+        printf("Failed to get time zone info.\n");
+    }
+
+    // Print bias (difference from UTC in minutes)
+    printf("UTC offset: %d minutes\n", -tzi.Bias); // Negative because Bias is added to local time
 }
