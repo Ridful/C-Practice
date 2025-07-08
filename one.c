5b2873a22718fcf3ec90e78a52ebf1065d813e63
Ridful <19477176+Ridful@users.noreply.github.com>
2025-07-08 22:33:34 +0200

    simple winapi msgbox

--- one.c
+++ one.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <Windows.h>
+/*
+*/
 
+// Prints a line to the screen
 void printLine(const char *line) {
     printf("%s\n", line);
 }
 
 int main() {
-    printLine("Hello, world!");
+
+    char *message = "Welcome, what's your name?";
+    char name[50];
+
+    printLine(message);
+    scanf("%s", &name);
+
+    printf("Hello, %s!\n", name);
+    // Call the printLine function with a string literal
+    printLine("\nHello, world!");
+
+    // Show a Windows message box
+    MessageBoxA(NULL, "This is a Windows API MessageBox!", "Hello from Windows API", MB_OK | MB_ICONINFORMATION);
+
     return 0;
 }
