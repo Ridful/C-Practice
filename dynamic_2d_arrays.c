20a623bc8a55b9b8205e5e51b2c8dd7e38282c5d
Ridful <19477176+Ridful@users.noreply.github.com>
2025-07-12 22:30:05 +0200

    dynamic memory allocation for 2d arrays

---
+++ dynamic_2d_arrays.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int nrows = 2;
+    int ncols = 5;
+    int i, j;
+
+    char **pvowels = (char **) malloc(nrows * sizeof(char *));
+
+    pvowels[0] = (char *) malloc(ncols * sizeof(char));
+    pvowels[1] = (char *) malloc(ncols * sizeof(char));
+
+    //a e i o u
+    pvowels[0][0] = 'A';
+    pvowels[0][1] = 'E';
+    pvowels[0][2] = 'I';
+    pvowels[0][3] = 'O';
+    pvowels[0][4] = 'U';
+
+    pvowels[1][0] = 'a';
+    pvowels[1][1] = 'e';
+    pvowels[1][2] = 'i';
+    pvowels[1][3] = 'o';
+    pvowels[1][4] = 'u';
+
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            printf("%c ", pvowels[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(pvowels[0]);
+    free(pvowels[1]);
+    free(pvowels);
+    return 0;
+}
