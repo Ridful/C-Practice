81861f121f7bf02f037dbb9f5dcc743f771ba9cd
Ridful <19477176+Ridful@users.noreply.github.com>
2025-07-14 23:52:59 +0200

    use recursion to print a string in reverse

--- recursion.c
+++ recursion.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 
 unsigned int multiply(unsigned int x, unsigned int y) {
     if (x == 1) { return y; }
@@ -9,10 +9,22 @@ unsigned int multiply(unsigned int x, unsigned int y) {
     return 0;
 }
 
+void reverse_string(char * word) {
+    if (*word == '\0') {
+        return;
+    }
+    reverse_string(word + 1);
+    putchar(*word);
+}
+
 
 int main() {
 
-    printf("3 times 5 is %d", multiply(3, 5));
+    printf("3 times 5 is %d\n", multiply(3, 5));
+
+    char * word = "hello";
+    reverse_string(word);
+    putchar('\n');
 
     return 0;
 }
